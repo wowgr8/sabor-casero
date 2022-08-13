167842d847b7d1063ac1a3c8cbae98d38f7d249c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-12 21:11:35 -0700

    add notes and func component with placeholder

---
+++ src/components/Gallery.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// This component may be refactored and combined with Menu.js - WIP
+
+function Gallery() {
+  return (
+    <div>Gallery</div>
+  )
+}
+
+export default Gallery
