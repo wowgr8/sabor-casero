288c5704bb63ed55c2853fae2b83bfb3447f7360
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-12 21:11:40 -0700

    add notes and func component with placeholder

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// Will act as landing page. Showing logo, About (Mexican & Salvadoran), and image of owner.
+
+function Hero() {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
