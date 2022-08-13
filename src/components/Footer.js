6e487bdbf0011942cd9cd504c195b0e83d039229
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-12 21:02:07 -0700

    add notes and func component with placeholder

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// will show copyright and created by wowgr8
+  // link to foodCart social media
+  // possibly hours and address (Visit.js may cover this.)
+
+function Footer() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
