42b8716395934df92ceda0f6bf5a82dd38688199
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-12 21:11:49 -0700

    add notes and func component with placeholder

---
+++ src/components/Visit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import snazzy maps
+// display hours and address
+// StretchGoal: add third party link eg. "order on grubhub".
+
+function Visit() {
+  return (
+    <div>Visit</div>
+  )
+}
+
+export default Visit
