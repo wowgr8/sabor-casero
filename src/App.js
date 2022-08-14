39ac25afaf6ae136f68f173573188a0636c1d3c1
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-13 17:39:30 -0700

    add import statements for all components and added component tags to return()

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Menu from './components/Menu';
+import Visit from './components/Visit';
+import Gallery from './components/Gallery';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Hero />
+      <Menu />
+      <Visit />
+      <Gallery />
+      <Footer />
     </div>
   );
 }
