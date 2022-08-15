040112d1b3d2b9c32b056115a1ddbbd67e8a125b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-14 20:36:14 -0700

    add import statement for smooth scroll and created navbar with links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,39 @@
-import React from 'react';
-// will handle smooth scroll.
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
 
 function Navbar() {
+
+  const [click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+
+  const closeMenu = () => setClick(false)
+
   return (
-    <div>Navbar</div>
+    <div className='header'>
+      <nav className='navbar'>
+        {/* <a href='/' className='logo'>
+          <img src={logo} alt='logo' />
+        </a> */}
+        <div className='hamburger' onClick={handleClick}>
+          {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
+          : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className='nav-item'>
+            <a href='/' onClick={closeMenu}>Home</a>
+          </li>
+          <li className='nav-item'>
+            <a href='#about' onClick={closeMenu}>Menu</a>
+          </li>
+          <li className='nav-item'>
+            <a href='#testimonials' onClick={closeMenu}>Visit</a>
+          </li>
+          <li className='nav-item'>
+            <a href='#demo' onClick={closeMenu}>Gallery</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
