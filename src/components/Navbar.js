44ba4d32688b2077d7af47107f560c0c5b68d6a3
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-16 22:12:39 -0700

    add indentation fix for Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,16 +22,16 @@ function Navbar() {
         </div> */}
         <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className='nav-item'>
-            <Link to ="hero" spy ={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Home</Link>
+            <Link to="hero" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Home</Link>
           </li>
           <li className='nav-item'>
-            <Link to ="menu" spy ={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Menu</Link>
+            <Link to="menu" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Menu</Link>
           </li>
           <li className='nav-item'>
-            <Link to ="visit" spy ={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Visit</Link>
+            <Link to="visit" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Visit</Link>
           </li>
           <li className='nav-item'>
-            <Link to ="gallery" spy ={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Gallery</Link>
+            <Link to="gallery" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Gallery</Link>
           </li>
         </ul>
       </nav>
