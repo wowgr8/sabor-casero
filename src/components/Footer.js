f004b345e3986523e933f5d0c0c8be20387ceeec
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-08-17 23:30:17 -0700

    add social media links and icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../styles/Footer.css';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { AiOutlineMail } from 'react-icons/ai';
   // link to foodCart social media
   // possibly hours and address (Visit.js may cover this.)
 
@@ -7,7 +9,9 @@ import '../styles/Footer.css';
     return (
       <div className='footer'>
         <div className='socialMedia'>
-
+          <a href='https://github.com/wowgr8'><BsGithub /></a>
+          <a href='https://www.linkedin.com/in/cesar-aug-lopez/'><BsLinkedin /></a>
+          <a href='mailto:lopez.cesar.aug@gmail.com'><AiOutlineMail /></a>
         </div>
         <p> &copy; 2022 github.com/wowgr8</p>
       </div>
