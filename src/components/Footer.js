d72841b85f4f2d619da8ce6f2e6f62825ef6da00
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-09-24 22:27:33 -0700

    add unordered list for link icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,17 @@ import { AiOutlineMail } from 'react-icons/ai';
   function Footer() {
     return (
       <div className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
-
+        <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
+          <li>
+            <a href='https://github.com/wowgr8' className="mr-4 hover:underline md:mr-6"><BsGithub /></a>
+          </li>
+          <li>
+            <a href='https://www.linkedin.com/in/cesar-aug-lopez/' className="mr-4 hover:underline md:mr-6"><BsLinkedin /></a>
+          </li>
+          <li>
+            <a href='mailto:lopez.cesar.aug@gmail.com' className="mr-4 hover:underline md:mr-6"><AiOutlineMail /></a>            
+          </li>
+        </ul>
         <span className="text-sm text-gray-500 sm:text-center">&copy; 2022 <a href='https://github.com/wowgr8' className="hover:underline">github.com/wowgr8™</a>. All Rights Reserved.
         </span>
       </div>
