c20638362de4d22ca6403106f9ac5bdad4f60e73
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-09-25 21:17:12 -0700

    add navigation tabs for menu, visit, and gallery. Remove Navbar.css import statement.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
-// import '../styles/Navbar.css';
 // add import for icons and logo
 
 function Navbar() {
@@ -18,9 +17,23 @@ function Navbar() {
           <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Sabor Casero Logo" />
           <span className="self-center text-xl font-semibold whitespace-nowrap">Sabor Casero</span>
         </a>
-
-
-
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+            {/* PLACEHOLDER FOR HOME/HERO */}
+            {/* <li>
+              <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</a>
+            </li> */}
+            <li>
+              <a href="#menu" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" onClick={closeMenu}>Menu</a>
+            </li>
+            <li>
+              <a href="#visit" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" onClick={closeMenu}>Visit</a>
+            </li>
+            <li>
+              <a href="#gallery" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" onClick={closeMenu}>Gallery</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
