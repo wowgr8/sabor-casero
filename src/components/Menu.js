58b647485f255f054f7a478e7ba7bd08145d61ce
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-09-28 17:24:52 -0700

    add temporary full page styling  and replace id with name attribute for smooth scroll functionality.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 function Menu() {
-  return (
-    <div>Menu</div>
+  return ( // Full width 
+  <div name='menu' className='snap-y snap-mandatory w-screen h-screen overflow-scroll'>
+    <div className="bg-amber-200 w-screen h-screen flex">Menu</div>
+  </div>
   )
 }
 
