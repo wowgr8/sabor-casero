a0f57b6645ad364296f8ddc3919fccdaf431f426
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-02 20:20:20 -0700

    add import statement for pupusasHD stock image. Add img element - WIP

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import quesaBirriaHD from "../assets/img/food-stockimg/quesa-birria-HD.jpg";
+import pupusasHD from "../assets/img/food-stockimg/pupusasHD.jpg";
 // Will act as landing page. Showing logo, About (Mexican & Salvadoran origins), and image of owner.
 
 function Hero() {
   return (
-    <div name="hero" className='bg-white w-screen h-screen flex '>
+    <div name="hero" className='bg-white w-screen h-screen flex'>
       <div className='grid grid-cols-1 sm:grid-cols-2 w-full'>
-          <div className="bg-red-200">pic</div>
-          {/* <img className="object-cover max-h-[10]" src={quesaBirriaHD} /> */}
-          <div className="bg-amber-200">words</div>
-          <div className="bg-cyan-200">words</div>
-          <div className="bg-fuchsia-200">pic</div>
+          <div className="bg-red-200 h-full">pic1</div>
+          {/* <img className="object-cover h-full" src={quesaBirriaHD} /> */}
+          <div className="bg-white h-full">words 1</div>
+          <div className="bg-white h-full">words 2</div>
+          {/* <div className="bg-fuchsia-200">pic2</div> */}
+          <img className="object-cover h-full" src={pupusasHD} />
       </div>
     </div>
   )
