1dc8f91b2a7bd2b91c13d74be7f8e8b81e5605a3
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-03 10:47:50 -0700

    add min-h to img and removed div container - images are now contained properly. Remove unused notes and comments.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,25 +6,16 @@ import pupusasHD from "../assets/img/food-stockimg/pupusasHD.jpg";
 function Hero() {
   return (
     <div name="hero" className='bg-white w-screen h-screen flex flex-wrap'>
-      {/* Next you can try all divs inside one grid div with grid-cols-2 and each div can have a grid-row-? */}
       <div className='grid grid-cols-1 sm:grid-cols-2 w-full h-1/2'>        
-        <div className='  bg-red-800'>  <img className="object-cover" src={quesaBirriaHD} />      </div>
+        <img className="w-full object-cover min-h-0 h-full" src={quesaBirriaHD} />
         <div className=' bg-red-700'>   <span>Placeholder for short origin story</span>     </div>
       </div>
       <div className='grid grid-cols-1 sm:grid-cols-2 w-full h-1/2'>
         <div className='bg-red-500'>  <span>Placeholder for hours</span>   </div>
-        <img className="object-none object-left " src={pupusasHD} />        
-        {/* object-anythingOtherThanNone does not work....., object-left only works with object-none atm.... */}
+        <img className="w-full object-cover min-h-0 h-full" src={pupusasHD} />        
       </div>
     </div>
   )
 }
 
 export default Hero
-
-// <div className="bg-red-200">pic1</div>
-// {/* <img className="object-cover w-full h-1/2 " src={quesaBirriaHD} /> */}
-// <div className="bg-red-400 ">words 1</div>
-// <div className="bg-orange-200 ">words 2</div>
-// {/* <div className="bg-fuchsia-200">pic2</div> */}
-// 
