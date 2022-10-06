abce71bc32356b1580ae7c05a40cc40c93069f70
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-06 15:38:46 -0700

    add first set of import statements for images and add them to GalleryList. Add name, image, and alt attributes to each image.

--- src/helpers/GalleryList.js
+++ src/helpers/GalleryList.js
@@ -1,24 +1,35 @@
-// Import all images images and add to array. 
+import AguaFresca from '../assets/img/food-facebook/agua-fresca-google.jpg';
+import BirriaConsomme from '../assets/img/food-facebook/birria-y-consomme-yelp.jpeg';
+import CocaConsomme from '../assets/img/food-facebook/consomme-cocacola-google.jpeg';
+import DunkConsomme from '../assets/img/food-facebook/dunk-consomme-yelp.jpeg';
+import MenuIRL from '../assets/img/food-facebook/menu-irl-google.jpg';
+
+// Import all images from food-facebook and add to array. 
 
 export const GalleryList = [
   {
-    name: "Placeholder.jpg",
+    name: "Agua Fresca",
+    image: AguaFresca,
+    alt: "Close up of a fruit juice"
+  },
+  {
+    name: "Birria con Consomme",
+    image: BirriaConsomme,
     alt: "alt attribute placeholder"
   },
   {
-    name: "Placeholder.jpg",
-    alt: "alt attribute placeholder"
+    name: "Coca-Cola con Consomme",
+    image: CocaConsomme,
+    alt: "Close-up of a Mexican coca-cola bottle and consomme stock"
   },
   {
-    name: "Placeholder.jpg",
-    alt: "alt attribute placeholder"
+    name: "Birria con Consomme",
+    image: DunkConsomme,
+    alt: "Close-up of Beef birria and consomme stock"
   },
   {
-    name: "Placeholder.jpg",
-    alt: "alt attribute placeholder"
-  },
-  {
-    name: "Placeholder.jpg",
-    alt: "alt attribute placeholder"
+    name: "Food Truck Menu",
+    image: MenuIRL,
+    alt: "Close-up of food prices and images"
   },
 ]
