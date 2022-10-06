240065127cd6ad4134c90fe3edc4148c9d6b4974
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-05 22:20:31 -0700

    add note and placeholder for array of image objects.

---
+++ src/helpers/GalleryList.js
@@ -0,0 +1,24 @@
+// Import all images images and add to array. 
+
+export const GalleryList = [
+  {
+    name: "Placeholder.jpg",
+    alt: "alt attribute placeholder"
+  },
+  {
+    name: "Placeholder.jpg",
+    alt: "alt attribute placeholder"
+  },
+  {
+    name: "Placeholder.jpg",
+    alt: "alt attribute placeholder"
+  },
+  {
+    name: "Placeholder.jpg",
+    alt: "alt attribute placeholder"
+  },
+  {
+    name: "Placeholder.jpg",
+    alt: "alt attribute placeholder"
+  },
+]
