40769e2ebe57e3d6992a2c0ec622524e0de9a963
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-07 20:17:45 -0700

    add commit before restarting local device. No big changes - WIP

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function Menu() {
-  return ( // Full width 
-  <div name='menu' className='snap-y snap-mandatory w-screen h-screen overflow-scroll'>
-    <div className="bg-amber-200 w-screen h-screen flex">Menu</div>
+  return (
+  <div name='menu' className='bg-amber-200 w-screen h-screen flex'>
+    <div className="">Menu</div>
   </div>
   )
 }
