2a3d748e2bbfbfa7e0007d7694b2c92a0f0d01c4
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-07 20:17:01 -0700

    add commit before restarting local device - WIP

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,12 @@
 import React from 'react';
-// This component may be refactored and combined with Menu.js - WIP
+// Import GalleryList.js
+// Import GalleryCarousel.js
 
 function Gallery() {
   return (
     <div name="gallery" className='bg-fuchsia-200 w-screen h-screen flex'>
       <div className="">Gallery</div>
+
     </div>
   )
 }
