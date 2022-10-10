7d5ec3c52da49628f1053b88a22b5b433ab86464
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-10 15:47:32 -0700

    add import statement for GalleryCarousel and GalleryList. Add loop of GalleryList and passed values as props to GalleryCarousel tag. - WIP

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,19 @@
 import React from 'react';
-// Import GalleryList.js
-// Import GalleryCarousel.js
+import GalleryCarousel from './GalleryCarousel';
+import { GalleryList } from '../helpers/GalleryList';
+
+console.log(GalleryList.map((picture, index) => {return picture.name}));
 
 function Gallery() {
   return (
-    <div name="gallery" className='bg-fuchsia-200 w-screen h-screen flex'>
-      <div className="">Gallery</div>
+    <div name="gallery" className='bg-fuchsia-200  flex'>
+      <div className="">Gallery</div> 
+      <div className="">
+        {GalleryList.map((picture, index) => {
+          return <GalleryCarousel id={index} name={picture.name} image={picture.image} alt={picture.alt}/> 
+        })}
+      </div> 
+      
 
     </div>
   )
