149a1452bf650f14480971e5f3790b091a0b2219
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-10 15:46:13 -0700

    add carousel component and passed in GalleryList props from Gallery.js - WIP

--- src/components/GalleryCarousel.js
+++ src/components/GalleryCarousel.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
-function GalleryCarousel() {
+
+function GalleryCarousel({ image, name, id, alt }) {
   return (
-    <div>GalleryCarousel</div>
+    <div id="controls-carousel" className="relative bg-fuchsia-800 w-screen h-screen flex" data-carousel="static">
+        {/* <!-- Carousel wrapper --> */}
+        <span>CAROUSEL</span>
+        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+            {/* <!-- Item 1 // LOOP THROUGH GalleryList--> */}
+            <div className="duration-700 ease-in-out absolute inset-0 transition-all transform -translate-x-full z-10" data-carousel-item="">
+                <img src={image} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt={alt} />
+                <span>{console.log(name, alt)}</span>
+            </div>
+        </div>
+        {/* <!-- Slider controls --> */}
+        <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev="">
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                <svg aria-hidden="true" className="w-6 h-6 text-white dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+                <span className="sr-only">Previous</span>
+            </span>
+        </button>
+        <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next="">
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                <svg aria-hidden="true" className="w-6 h-6 text-white dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+                <span className="sr-only">Next</span>
+            </span>
+        </button>
+    </div>
+
   )
 }
 
