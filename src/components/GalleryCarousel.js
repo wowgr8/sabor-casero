f8a4f4553e5b8f8e7de4ef261a4a2e740a2bef3d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-09 20:38:19 -0800

    add useState and react icons import statement. add count variable and useState currentIndex and setCurrentIndex state setter with initial state of 0.

--- src/components/GalleryCarousel.js
+++ src/components/GalleryCarousel.js
@@ -1,40 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { GalleryList } from '../helpers/GalleryList';
+import { AiOutlineVerticalRight, AiOutlineVerticalLeft } from "react-icons/ai";
 
+let count = 0;
 
 function GalleryCarousel() {
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  return (
-    <div id="controls-carousel" className="relative bg-fuchsia-800 w-screen h-screen flex" data-carousel="static">
-        {/* <!-- Carousel wrapper --> */}
-        <span>CAROUSEL</span>
-        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-        <span>CAROUSEL</span>
-            {/* <!-- Item 1 // LOOP THROUGH GalleryList--> */}
-            <div className="duration-700 ease-in-out absolute inset-0 transition-all transform -translate-x-full z-10" data-carousel-item="">
-        <span>CAROUSEL</span>
-                <img src={GalleryList.map((picture, index) => {return picture.image})} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"  />
-                <span>{GalleryList.map((picture, index) => {return picture.name})}</span>
-                <span>CAROUSEL</span>
-            </div>
-        </div>
-        {/* <!-- Slider controls --> */}
-        <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev="">
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-                <svg aria-hidden="true" className="w-6 h-6 text-white dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-                <span className="sr-only">Previous</span>
-            </span>
-        </button>
-        <span>CAROUSEL</span>
-        <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next="">
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-                <svg aria-hidden="true" className="w-6 h-6 text-white dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-                <span className="sr-only">Next</span>
-            </span>
-        </button>
+    console.log(GalleryList.map((picture, index) => {return picture.name}))
+
+
+
+    return (
+    <div className="w-full select-none relative">
+
     </div>
 
-  )
+    )
 }
 
 export default GalleryCarousel
