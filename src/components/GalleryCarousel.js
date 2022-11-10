36e6973ff79079dadcfa3dd34f4ca96156f64b4b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-09 20:45:38 -0800

    remove console.log() and fix indentation.

--- src/components/GalleryCarousel.js
+++ src/components/GalleryCarousel.js
@@ -7,8 +7,6 @@ let count = 0;
 function GalleryCarousel() {
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    console.log(GalleryList.map((picture, index) => {return picture.name}))
-
     const handleOnNextClick = () => {
         count = (count + 1) % GalleryList.length;
         setCurrentIndex(count);
@@ -21,27 +19,26 @@ function GalleryCarousel() {
     };
 
     return (
-    <div className="w-full select-none relative">
-        <div className="aspect-w-16 aspect-h-9">
-            <img src={GalleryList[currentIndex].image} alt={GalleryList[currentIndex].alt} />
-        </div>
+        <div className="w-full select-none relative">
+            <div className="aspect-w-16 aspect-h-9">
+                <img src={GalleryList[currentIndex].image} alt={GalleryList[currentIndex].alt} />
+            </div>
 
-        <div className="absolute w-full top-1/2 transform -translate-y-1/2 px-3 flex justify-between items-center">
-            <button
-                className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
-                onClick={handleOnPrevClick}
-            >
-                <AiOutlineVerticalRight size={30} />
-            </button>
-            <button
-                className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
-                onClick={handleOnNextClick}
-            >
-                <AiOutlineVerticalLeft size={30} />
-            </button>
+            <div className="absolute w-full top-1/2 transform -translate-y-1/2 px-3 flex justify-between items-center">
+                <button
+                    className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
+                    onClick={handleOnPrevClick}
+                >
+                    <AiOutlineVerticalRight size={30} />
+                </button>
+                <button
+                    className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
+                    onClick={handleOnNextClick}
+                >
+                    <AiOutlineVerticalLeft size={30} />
+                </button>
+            </div>
         </div>
-    </div>
-
     )
 }
 
