8ead62a66059c9326ff252e053d09e916aad6f8d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-27 18:08:54 -0800

    add image objects to list

--- src/helpers/GalleryList.js
+++ src/helpers/GalleryList.js
@@ -3,6 +3,12 @@ import BirriaConsomme from '../assets/img/food-facebook/birria-y-consomme-yelp.j
 import CocaConsomme from '../assets/img/food-facebook/consomme-cocacola-google.jpg';
 import DunkConsomme from '../assets/img/food-facebook/dunk-consomme-yelp.jpeg';
 import MenuIRL from '../assets/img/food-facebook/menu-irl-google.jpg';
+import MicroMenu from '../assets/img/food-facebook/micro-menu-yelp.jpeg';
+import Platanos from '../assets/img/food-facebook/platanos-yelp.jpeg';
+import CamaronTaco from '../assets/img/food-facebook/plated-camaron-taco-google.jpg';
+import Pupusa from '../assets/img/food-facebook/plated-oval-pupusa-google.jpg';
+import Pupusa1 from '../assets/img/food-facebook/plated-pupusa-google.jpg';
+import TacoYPupusa from '../assets/img/food-facebook/plated-tacos-y-pupusa-google.jpg';
 
 // Import all images from food-facebook and add to array. 
 
@@ -31,5 +37,35 @@ export const GalleryList = [
     name: "Food Truck Menu",
     image: MenuIRL,
     alt: "Close-up of food prices and images"
+  },
+  {
+    name: "Food Truck Menu v2",
+    image: MicroMenu,
+    alt: "Close-up of food prices and images"
+  },
+  {
+    name: "Platanos",
+    image: Platanos,
+    alt: "Close-up of platanos dish"
+  },
+  {
+    name: "Taco de Camaron",
+    image: CamaronTaco,
+    alt: "Shrimp Taco"
+  },
+  {
+    name: "Pupusa",
+    image: Pupusa,
+    alt: "A pupusa on a plate"
+  },
+  {
+    name: "Two pupusas",
+    image: Pupusa1,
+    alt: "Two pupusas on a plate"
+  },
+  {
+    name: "Tacos con pupusa",
+    image: TacoYPupusa,
+    alt: "Two tacos and a pupusa on a plate"
   },
 ]
