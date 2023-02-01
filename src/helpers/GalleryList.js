b883520fdc1ab320736815f9460d6c356e36a523
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-31 19:37:34 -0800

    add image objects to list

--- src/helpers/GalleryList.js
+++ src/helpers/GalleryList.js
@@ -11,6 +11,9 @@ import Pupusa1 from '../assets/img/food-facebook/plated-pupusa-google.jpg';
 import TacoYPupusa from '../assets/img/food-facebook/plated-tacos-y-pupusa-google.jpg';
 import TacoYTamale from '../assets/img/food-facebook/plated-tamale-taco-google.jpg';
 import PupusaTogo from '../assets/img/food-facebook/pupusa-togo-yelp.jpeg';
+import OutsideDining from '../assets/img/food-facebook/quesabirria-outdoortable-yelp.jpeg';
+import QuesaTacoCloseUp from '../assets/img/food-facebook/quessabirria-closeup-yelp.jpeg';
+import TogoQuesaTacoCloseUp from '../assets/img/food-facebook/quessabirria-togo-yelp.jpeg';
 
 // Import all images from food-facebook and add to array. 
 
@@ -79,5 +82,20 @@ export const GalleryList = [
     name: "Pupusas ToGo",
     image: PupusaTogo,
     alt: "Pupusas in a to-go container"
+  },
+  {
+    name: "Outdoor Table",
+    image: OutsideDining,
+    alt: "Quesa Birria on an outdoor bench/eating area"
+  },
+  {
+    name: "Quesa Birra Closeup",
+    image: QuesaTacoCloseUp,
+    alt: "A close up shot of quesa-birria with consomme"
+  },
+  {
+    name: "To-go Quesa Birra Closeup",
+    image: TogoQuesaTacoCloseUp,
+    alt: "A close up shot of quesa-birria with consomme in to-go boxes"
   },
 ]
