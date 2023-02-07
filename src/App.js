49df09607e7edf3fe8be0becb216290545f7cd6b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-02-06 17:48:08 -0800

    remove Visit, Hero, Gallery, and Menu components and replace with Control component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Menu from './components/Menu';
-import Visit from './components/Visit';
-import Gallery from './components/Gallery';
 import Footer from './components/Footer';
+import Control from './components/Control';
 
 function App() {
   return (
     <div className="App max-w-screen-xl absolute">
       <Navbar />
-      <Hero />
-      <Menu />
-      <Visit />
-      <Gallery />
+      <Control />
       <Footer />
     </div>
   );
