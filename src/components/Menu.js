c43173a8c06cb3f555fabd0fbc0f21b810a7c5b2
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-06-21 22:24:28 -0700

    remove relative position

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Menu() {
   return (
-  <section name='menu' className='bg-amber-200 w-screen h-screen relative'>
+  <section name='menu' className='bg-amber-200 w-screen h-screen '>
     <div className="text-center ">Menu</div>
   </section>
   )
